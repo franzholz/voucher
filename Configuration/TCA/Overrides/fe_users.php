e05fe734a37b1bd64f523349f33dc4d4f0be5918
Franz Holzinger <holzinger@ttproducts.de>
2020-10-02 18:17:33 +0200

    rebuild the Overrides based on a function

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -1,27 +1,32 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-$table = 'fe_users';
+call_user_func(function($extKey) {
 
-$temporaryColumns =
-    array (
-        'tx_voucher_usedcode' => array (
-            'exclude' => 1,
-            'label' => VOUCHER_EXT_LANGUAGE_PATH . 'locallang_db.xml:' . $table . '.tx_voucher_usedcode',
-            'config' => array (
-                'type' => 'input',
-                'size' => '20',
-                'max' => '256',
-                'readOnly' => '1',
+    $table = 'fe_users';
+
+    $temporaryColumns =
+        array (
+            'tx_voucher_usedcode' => array (
+                'exclude' => 1,
+                'label' => VOUCHER_EXT_LANGUAGE_PATH . 'locallang_db.xml:' . $table . '.tx_voucher_usedcode',
+                'config' => array (
+                    'type' => 'input',
+                    'size' => '20',
+                    'max' => '256',
+                    'readOnly' => '1',
+                )
             )
-        )
-    );
+        );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $temporaryColumns);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-    $table,
-    'tx_voucher_usedcode;;;;1-1-1',
-    '',
-    'after:www'
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $temporaryColumns);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        $table,
+        'tx_voucher_usedcode;;;;1-1-1',
+        '',
+        'after:www'
+    );
+}, VOUCHER_EXT);
+
+
 
--- Configuration/TCA/Overrides/tx_voucher_codes.php
+++ Configuration/TCA/Overrides/tx_voucher_codes.php
@@ -1,28 +1,29 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-$table = 'tx_voucher_codes';
+call_user_func(function($extKey) {
+    $table = 'tx_voucher_codes';
 
-$temporaryColumns =
-    array (
-        'code' => array (
-            'exclude' => 1,
-            'label' => VOUCHER_EXT_LANGUAGE_PATH . 'locallang_db.xlf:' . $table . '.code',
-            'config' => array (
-                'type' => 'input',
-                'size' => $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['codeSize'],
-                'max' => $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['codeSize'],
-                'eval' => 'required,trim',
+    $temporaryColumns =
+        array (
+            'code' => array (
+                'exclude' => 1,
+                'label' => VOUCHER_EXT_LANGUAGE_PATH . 'locallang_db.xlf:' . $table . '.code',
+                'config' => array (
+                    'type' => 'input',
+                    'size' => $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extKey]['codeSize'],
+                    'max' => $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extKey]['codeSize'],
+                    'eval' => 'required,trim',
+                )
             )
-        )
+        );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $temporaryColumns);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        $table,
+        'code',
+        '',
+        'after:title'
     );
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $temporaryColumns);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-    $table,
-    'code',
-    '',
-    'after:title'
-);
-
+}, VOUCHER_EXT);
 
