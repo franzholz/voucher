2ea4c815dd432d3c1baccd50343807600517d7d5
Franz Holzinger <holzinger@ttproducts.de>
2020-10-16 23:03:26 +0200

    restructure ext_localconf.php and ext_tables.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,55 +1,61 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-define('VOUCHER_EXT', 'voucher');
+call_user_func(function () {
 
-if (!defined ('VOUCHER_EXT_LANGUAGE')) {
-    define('VOUCHER_EXT_LANGUAGE_PATH', 'LLL:EXT:' . VOUCHER_EXT . '/Resources/Private/Language/');
-}
+    define('VOUCHER_EXT', 'voucher');
 
-$_EXTCONF = unserialize($_EXTCONF);    // unserializing the configuration so we can use it here:
-
-if (
-    isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]) &&
-    is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT])
-) {
-    $tmpArray = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT];
-} else if (isset($tmpArray)) {
-    unset($tmpArray);
-}
-
-if (isset($_EXTCONF) && is_array($_EXTCONF)) {
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT] = $_EXTCONF;
-    if (isset($tmpArray) && is_array($tmpArray)) {
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT] =
-            array_merge($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT], $tmpArray);
+    if (!defined ('VOUCHER_EXT_LANGUAGE')) {
+        define('VOUCHER_EXT_LANGUAGE_PATH', 'LLL:EXT:' . VOUCHER_EXT . '/Resources/Private/Language/');
     }
-} else if (!isset($tmpArray)) {
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT] = [];
-}
 
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['codeSize'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['codeSize'] = 32;
-}
+    $_EXTCONF = unserialize($_EXTCONF);    // unserializing the configuration so we can use it here:
 
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['module'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['module'] = 1;
-}
+    if (
+        isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]) &&
+        is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT])
+    ) {
+        $tmpArray = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT];
+    } else if (isset($tmpArray)) {
+        unset($tmpArray);
+    }
+
+    if (isset($_EXTCONF) && is_array($_EXTCONF)) {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT] = $_EXTCONF;
+        if (isset($tmpArray) && is_array($tmpArray)) {
+            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT] =
+                array_merge($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT], $tmpArray);
+        }
+    } else if (!isset($tmpArray)) {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT] = [];
+    }
+
+    if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['codeSize'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['codeSize'] = 32;
+    }
+
+    if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['module'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['module'] = 1;
+    }
 
 
-if (
-    TYPO3\CMS\Core\Utility\GeneralUtility::inList(
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['hooks'],
-        'agency'
-    )
-) {
-    $hookExtension = 'agency';
-    $classPath = \JambageCom\Voucher\Agency\Agency::class;
+    if (
+        TYPO3\CMS\Core\Utility\GeneralUtility::inList(
+            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][VOUCHER_EXT]['hooks'],
+            'agency'
+        )
+    ) {
+        $hookExtension = 'agency';
+        $classPath = \JambageCom\Voucher\Agency\Agency::class;
 
-        // Agency marker hook
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$hookExtension]['registrationProcess'][] = $classPath;
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$hookExtension]['model'][] = $classPath;
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$hookExtension]['registrationProcess_afterSaveCreate'][] = $classPath;
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$hookExtension]['confirmRegistrationClass'][] = $classPath;
-}
+            // Agency marker hook
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$hookExtension]['registrationProcess'][] = $classPath;
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$hookExtension]['model'][] = $classPath;
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$hookExtension]['registrationProcess_afterSaveCreate'][] = $classPath;
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$hookExtension]['confirmRegistrationClass'][] = $classPath;
+    }
+
+});
+
+
 
